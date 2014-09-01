a293b924095f0523ed092da4693515bd69de789b
nypgand1 <nypgand1@gmail.com>
2014-09-01 13:56:37 +0800

    add all zones' coordinates to data in json

--- zone.py
+++ zone.py
@@ -1,6 +1,22 @@
 import json
 
-FILE_NAME_INPUT = {'center_8': './zone/center_8.json', 'center_8_16': './zone/center_8_16.json'}
+FILE_NAME_INPUT = dict()
+FILE_NAME_INPUT['center_08'] = './zone/center_08.json'
+FILE_NAME_INPUT['center_08_16'] = './zone/center_08_16.json'
+FILE_NAME_INPUT['center_16_24'] = './zone/center_16_24.json'
+FILE_NAME_INPUT['center_24'] = './zone/center_24.json'
+FILE_NAME_INPUT['left_08_16'] = './zone/left_08_16.json'
+FILE_NAME_INPUT['left_16_24'] = './zone/left_16_24.json'
+FILE_NAME_INPUT['left_24'] = './zone/left_24.json'
+FILE_NAME_INPUT['left_center_16_24'] = './zone/left_center_16_24.json'
+FILE_NAME_INPUT['left_center_24'] = './zone/left_center_24.json'
+FILE_NAME_INPUT['right_08_16'] = './zone/right_08_16.json'
+FILE_NAME_INPUT['right_16_24'] = './zone/right_16_24.json'
+FILE_NAME_INPUT['right_24'] = './zone/right_24.json'
+FILE_NAME_INPUT['right_center_16_24'] = './zone/right_center_16_24.json'
+FILE_NAME_INPUT['right_center_24'] = './zone/right_center_24.json'
+
+
 FILE_NAME_OUTPUT = 'shot_chart_zone.json'
 
 
@@ -14,11 +30,6 @@ def convert_to_xy(filename):
         xy_len = len(xy)
     
         for i in range(0, xy_len, 2):
-            print i
-            print xy[i]
-            print xy[i+1]
-            print '\n'
-    
             zone_xy.append([xy[i], xy[i+1]])
 
     return zone_xy
@@ -32,7 +43,7 @@ for name, filename in FILE_NAME_INPUT.iteritems():
     zone['properties'] = {'ID': len(features), 'ShopName': name}
     features.append(zone)
 
-    print json.dumps(zone, indent=4, separators=(',', ': '))
+    #print json.dumps(zone, indent=4, separators=(',', ': '))
 
 
 with open(FILE_NAME_OUTPUT) as f:
